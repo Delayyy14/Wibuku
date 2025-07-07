db7c8ca2c4c2448fe52fee39789cb8df5cbcb8e4
Delayyy14 <wahyusetiyo79@gmail.com>
2025-07-07 10:34:03 +0700

    tambah background signin dan filter blur

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import bg from '../image/bgSignin.jpg'; // ganti dengan path gambar kamu
+import elaina from '../image/elaina.jpg';
 
 function SignIn() {
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Di sini kamu bisa validasi login dulu kalau mau
-    navigate('/'); // pindah ke halaman utama
+    navigate('/');
   };
 
   return (
-    <div className="min-h-screen flex justify-center items-center bg-sky-200">
-      <div className="bg-white p-10 rounded-xl shadow-lg w-96">
-        <h2 className="text-2xl font-bold mb-5 text-center text-sky-800">Sign In</h2>
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <div className="bg-white/20 backdrop-blur-md p-10 rounded-xl shadow-lg w-96">
+      <img src="elaina" alt="hai" className='text-center'/>
+        <h2 className="text-2xl font-bold mb-5 text-center text-black drop-shadow">
+          Sign In
+        </h2>
         <form onSubmit={handleSubmit}>
+            
           <input
             type="email"
             placeholder="Email"
-            className="w-full px-4 py-2 mb-4 border rounded"
+            className="w-full px-4 py-2 mb-4 border rounded bg-white/30 text-black placeholder-black/70 backdrop-blur-sm"
             required
           />
           <input
             type="password"
             minLength={7}
             placeholder="Password"
-            className="w-full px-4 py-2 mb-4 border rounded"
+            className="w-full px-4 py-2 mb-4 border rounded bg-white/30 text-black placeholder-black/70 backdrop-blur-sm"
             required
           />
           <button
